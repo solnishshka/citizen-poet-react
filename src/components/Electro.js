4fa54ac0021a7b33c26bc7eb4ba1883f45b3674f
solnishshka <testsolnishshka@yandex.ru>
2020-12-20 11:18:54 +0300

    add api

--- src/components/Electro.js
+++ src/components/Electro.js
@@ -4,19 +4,20 @@ import qImage from "../images/progress-bar-3.png";
 import React from 'react';
 
 export default function Electro(props) {
-  const [ qoutes, setQuotes ] = React.useState([]);
+  const [qoutes, setQuotes] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("http://buymebuyme.xyz?q=электричество")
-  .then((result) => {
-    if (result.ok) {
-      return result.json();
-    } else return Promise.reject(`Что-то пошло не так: ${result.status}`);
-  }).then((data) => {
-    setQuotes(data);
-  }).catch((err) => {
-    console.log(err);
-  });
+    fetch("https://buymebuyme.xyz?q=электричество")
+      .then((result) => {
+        if (result.ok) {
+          return result.json();
+        } else return Promise.reject(`Что-то пошло не так: ${result.status}`);
+      }).then((data) => {
+        console.log(data);
+        setQuotes(data);
+      }).catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
@@ -28,7 +29,7 @@ export default function Electro(props) {
         progressBar={qImage}
       />
       <div className="table table_theme_problems">
-      {qoutes.map((item, i) => (
+        {qoutes.map((item, i) => (
           <Card key={i} className={"card__description card__description_theme_quotes"} cardText_1={item.fields.text.split("\n")[2]} cardText_2={item.fields.text.split("\n")[3]} cardText_3={item.fields.text.split("\n")[4]} cardText_4={item.fields.text.split("\n")[5]} />
         ))}
       </div>
