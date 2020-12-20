801c6aedd8929784a8489d3da48856fdcfc0219a
solnishshka <testsolnishshka@yandex.ru>
2020-12-20 20:48:06 +0300

    add page not found

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 function PageNotFound() {
   return (
     <div className="not-found">
-      <h3 className="not-found__title">
+      <h3>
         <span>404</span> - Страница не найдена
       </h3>
-      <p className="not-found__text">Скоро здесь будет контент</p>
+      <p>Скоро здесь будет контент</p>
       <Link to="/">
         <button className="button button_theme_apply">
           Вернуться на главную
