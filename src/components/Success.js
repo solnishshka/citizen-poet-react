99a7ba5fc036b6702a3f17895871380fcc4e0002
solnishshka <testsolnishshka@yandex.ru>
2020-12-20 19:53:04 +0300

    some fix

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,5 +1,5 @@
 import PageTemplate from "./PageTemplate";
-import qImage from "../images/progress-bar-4.png";
+import qImage from "../images/progress-bar-5.png";
 import { Link } from "react-router-dom";
 
 export default function Success(props) {
